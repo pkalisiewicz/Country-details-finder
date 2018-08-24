245d94a0af550eadfcd379c11fc5ae154049faa2
pkalisiewicz <przemyslaw.kalisiewicz@gmail.com>
2018-08-24 19:44:17 +0100

    add draft of svg map

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header :appInfo="appInfo" />
+    <InteractiveMap />
   </div>
 </template>
 
 <script>
 import { appInfo } from "./settings/settings.js";
 import Header from "./components/Header";
+import InteractiveMap from "./components/InteractiveMap";
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    InteractiveMap
   },
   data() {
     return {
