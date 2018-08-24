c22f2ed70762135024970a4b2f59f7e6b2cd5f3d
pkalisiewicz <przemyslaw.kalisiewicz@gmail.com>
2018-08-24 16:42:20 +0100

    add init settings

---
+++ src/settings/settings.js
@@ -0,0 +1,13 @@
+const BASE_URL = "https://restcountries.eu/rest/v2";
+
+const URL_SETTINGS = {
+  BASE_URL,
+  SEARCH_ALL: `${BASE_URL}/all`,
+  SEARCH_NAME: `${BASE_URL}/name/`,
+  SEARCH_CAPITAL: `${BASE_URL}/capital/`,
+  SEARCH_REGION: `${BASE_URL}/region/`
+};
+
+export default {
+  URL_SETTINGS
+};
