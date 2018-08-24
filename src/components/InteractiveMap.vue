9180884b5b313de8032bdd6007f839204b7cc290
pkalisiewicz <przemyslaw.kalisiewicz@gmail.com>
2018-08-24 23:44:33 +0100

    add example data and map click handling

--- src/components/InteractiveMap.vue
+++ src/components/InteractiveMap.vue
@@ -6,18 +6,56 @@
 
 <script>
 import WorldMap from "../assets/InteractiveMap.svg";
+import countriesData from "../assets/data.json";
 export default {
   name: "InteractiveMap",
   components: {
     WorldMap
   },
+  data() {
+    return {
+      countriesData
+    };
+  },
   mounted() {
-    const mapElements = Array.from(
-      document.querySelectorAll("#world-map path")
-    );
-    mapElements.forEach(path => {
-      path.classList.add("world-map__map-region");
-    });
+    this.addMapClickHandler();
+    this.addCountryClass();
+  },
+  methods: {
+    addMapClickHandler() {
+      const mapElement = document.querySelector("#world-map");
+      mapElement.addEventListener("click", this.openCountry);
+    },
+    addCountryClass() {
+      const mapElements = Array.from(
+        document.querySelectorAll("#world-map path")
+      );
+      mapElements.forEach(path => {
+        path.classList.add("world-map__map-region");
+      });
+    },
+    openCountry(e) {
+      if (!e.target.nodeName === "path") {
+        return false;
+      }
+      const selectedCountry = {
+        name: e.target.dataset.name,
+        code: e.target.dataset.id
+      };
+      const countryDetails = this.findCountryDetails(selectedCountry);
+      if (!countryDetails) {
+        return false;
+      }
+    },
+    findCountryDetails(selectedCountry) {
+      const resultOne = countriesData.find(
+        country => country.name === selectedCountry.name
+      );
+      const resultTwo = countriesData.find(
+        country => country.alpha2Code === selectedCountry.code
+      );
+      return resultOne ? resultOne : resultTwo;
+    }
   }
 };
 </script>
